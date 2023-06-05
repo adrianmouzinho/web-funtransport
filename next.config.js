f9fc042a59b25213c4c0986a7cf589fab43f50aa
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2023-06-04 21:53:43 -0300

    feat: add new image domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
-    domains: ['cdn.shopify.com', 'localhost', '192.168.1.125'],
+    domains: [
+      'cdn.shopify.com',
+      'm.media-amazon.com',
+      'localhost',
+      '192.168.1.125',
+    ],
   },
 }
 
