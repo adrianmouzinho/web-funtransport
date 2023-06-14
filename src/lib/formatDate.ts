ee2423ea6925f822659f1b466706681187d62a90
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2023-06-14 09:27:56 -0300

    add day an dmonth on formatDate function

--- src/lib/formatDate.ts
+++ src/lib/formatDate.ts
@@ -4,7 +4,7 @@ import ptBr from 'dayjs/locale/pt-br'
 dayjs.locale(ptBr)
 
 export function formatDate(date: string): string {
-  const formattedDate = dayjs(date).format('hh:mm A')
+  const formattedDate = dayjs(date).format('DD/MM hh:mm A')
 
   return formattedDate
 }
